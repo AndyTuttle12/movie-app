9e6ae6aeafe49621dd596aa1b895a5e610844de5
Andy Tuttle <andytuttle12@gmail.com>
2016-12-21 10:12:50 -0500

    Additional setup

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
 
+	// for all API calls:
+	var apiBaseUrl = 'http://api.themoviedb.org/3';
+	var imageBaseUrl = 'http://image.tndb.org/t/p';
+
+	const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key=' + apiKey + '&language=en-US&page=1';
+
 });
